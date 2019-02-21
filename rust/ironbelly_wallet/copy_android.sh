9cd63690748feb43e850ef25bb6af16960f80fdd
Liam Aharon <liam.aharon@hotmail.com>
2019-02-21 12:17:18 +1100

    Android build scaffold setup

---
+++ rust/ironbelly_wallet/copy_android.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mkdir -p ../../android/app/src/main/jniLibs
+mkdir -p ../../android/app/src/main/jniLibs/x86
+mkdir -p ../../android/app/src/main/jniLibs/arm64-v8a
+mkdir -p ../../android/app/src/main/jniLibs/armeabi-v7a
+
+cp ./target/i686-linux-android/release/libironbelly_wallet.so ../../android/app/src/main/jniLibs/x86/libironbelly_wallet.so
+cp ./target/aarch64-linux-android/release/libironbelly_wallet.so ../../android/app/src/main/jniLibs/arm64-v8a/libironbelly_wallet.so
+cp ./target/armv7-linux-androideabi/release/libironbelly_wallet.so ../../android/app/src/main/jniLibs/armeabi-v7a/libironbelly_wallet.so
